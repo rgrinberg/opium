506fa88610ae4d6dea6e38e7e0c956ee2c793e1e
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-10-15 01:28:38 -0400

    use queue instead of lists

--- lib/opium.ml
+++ lib/opium.ml
@@ -123,9 +123,9 @@ module App = struct
   }
 
   type t = {
-    before_filters : (Request.t -> Request.t Deferred.t) endpoint list;
-    routes : (Request.t -> Response.t Deferred.t) endpoint list;
-    after_filters : (Response.t -> Response.t Deferred.t) endpoint list;
+    before_filters : (Request.t -> Request.t Deferred.t) Queue.t;
+    routes : (Request.t -> Response.t Deferred.t) endpoint Method_bin.t;
+    after_filters : (Response.t -> Response.t Deferred.t) Queue.t;
     not_found : (Request.t -> Response.t Deferred.t);
     public_dir : Local_map.t option;
   }
@@ -133,17 +133,21 @@ module App = struct
   let local_path = Fn.compose Uri.path C.Request.uri
 
   let register app ~meth ~route ~action =
-    { app with routes=({meth;route;action}::app.routes) }
+    Method_bin.add app.routes meth {meth; route; action}
 
-  let app = 
+  let not_found not_found app = { app with not_found }
+
+  let app () = 
     let not_found req = 
       let path = req |> Request.raw |> local_path in
       Co.Server.respond_with_string ~code:`Not_found ("Not found: " ^ path)
     in
-    let public_dir = Local_map.(
-        Some { prefix="/public"; local_path="./public" })
-    in
-    { before_filters=[]; routes=[]; after_filters=[];
+    let public_dir =
+      let open Local_map in
+      Some { prefix="/public"; local_path="./public" } in
+    { before_filters=Queue.create ();
+      routes=Method_bin.create ();
+      after_filters=Queue.create ();
       public_dir; not_found }
 
   let public_path root requested =
@@ -151,18 +155,17 @@ module App = struct
     Option.some_if (String.is_prefix asked_path ~prefix:root) asked_path
 
   let matching_endpoint endpoints req uri =
-    endpoints |> List.find ~f:(fun {meth; route; action} ->
-        C.Request.meth req = meth
-        && Option.is_some @@ Route.match_url route uri)
+    let endpoints = Method_bin.get endpoints (C.Request.meth req) in
+    endpoints |> Queue.find_map ~f:(fun ep -> 
+      uri |> Route.match_url ep.route |> Option.map ~f:(fun p -> (ep, p)))
 
-  let server ?(port=3000) app = 
+  let server ?(port=3000) app =
     Co.Server.create ~on_handler_error:`Raise (Tcp.on_port port)
       (fun ~body sock req -> 
          let uri        = local_path req in
          let endpoint   = matching_endpoint app.routes req uri in
          match endpoint with
-         | Some {route;action;_} ->
-           let params = Option.value_exn (Route.match_url route uri) in 
+         | Some ({route;action;_}, params) ->
            action Request.({ raw=req; params })
          | None -> 
            let resp = 
@@ -189,8 +192,15 @@ module Std = struct
   let put route action =
     App.register ~meth:`PUT ~route:(Route.create route) ~action
 
+  let not_found = App.not_found
+  let app = App.app
+
+  let before action = ()
+  let after action = ()
+
   let start endpoints = 
-    let app = endpoints |> List.fold ~init:App.app ~f:(fun app e -> e app) in
+    let app = App.app () in
+    endpoints |> List.iter ~f:(fun e -> e app);
     app |> App.server |> ignore;
     Scheduler.go ()
 end
