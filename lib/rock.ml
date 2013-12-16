1663677d31b3faabfc7c5485d3e539955c12b928
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-15 23:13:32 -0800

    docs + formatting

--- lib/rock.ml
+++ lib/rock.ml
@@ -1,3 +1,6 @@
+(** A tiny clone of ruby's Rack protocol in OCaml. It's not imperative
+    to have this to for such a tiny framework but it makes extensions
+    a lot more straightforward *)
 open Core.Std
 open Async.Std
 open Cohttp
@@ -19,7 +22,7 @@ module Response = struct
     code: Code.status_code;
     headers: Header.t;
     body: string Pipe.Reader.t;
-  }
+  } with fields
 
   let default_header = Option.value ~default:(Header.init ())
 
@@ -36,12 +39,11 @@ end
 module Handler = struct
   type t = Request.t -> Response.t Deferred.t
   let call app req = app req
-      
+
   let default _ = return @@ Response.create ()
   let not_found _ =
     return @@ Response.string_body ~code:`Not_found
       "<html><body><h1>404 - Not found</h1></body></html>"
-
 end
 
 module Middleware = struct
@@ -55,13 +57,13 @@ module App = struct
   type t = {
     middlewares: Middleware.t list;
     handler: Handler.t
-  }
+  } with fields
 
-  let create ?(middlewares=[]) ~handler =
-    { middlewares; handler }
+  let create ?(middlewares=[]) ~handler = { middlewares; handler }
 
   let run { handler; middlewares } ~port =
     let module Server = Cohttp_async.Server in
+    let middlewares = List.rev middlewares in
     Server.create ~on_handler_error:`Raise (Tcp.on_port port)
       begin fun ~body sock req ->
         let req = Request.create req in
