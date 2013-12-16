f1c710b5f31f0079e59b6e3b6797b261457a91f5
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-15 23:12:04 -0800

    clean up mli for rock

--- lib/rock.mli
+++ lib/rock.mli
@@ -1,7 +1,12 @@
+open Core.Std
+open Async.Std
 
 module Request : sig
-  type t = { request : Cohttp.Request.t; env : Core.Std.Univ_map.t; } with fields
-  val create : ?env:Core.Std.Univ_map.t -> Cohttp.Request.t -> t
+  type t = {
+    request : Cohttp.Request.t;
+    env : Univ_map.t;
+  } with fields
+  val create : ?env:Univ_map.t -> Cohttp.Request.t -> t
   val uri : t -> Uri.t
   val meth : t -> Cohttp.Code.meth
 end
@@ -10,11 +15,11 @@ module Response : sig
   type t = {
     code : Cohttp.Code.status_code;
     headers : Cohttp.Header.t;
-    body : string Async.Std.Pipe.Reader.t;
-  }
-  val default_header : Cohttp.Header.t Core.Std.Option.t -> Cohttp.Header.t
+    body : string Pipe.Reader.t;
+  } with fields
+  val default_header : Cohttp.Header.t Option.t -> Cohttp.Header.t
   val create :
-    ?body:string Async.Std.Pipe.Reader.t ->
+    ?body:string Pipe.Reader.t ->
     ?headers:Cohttp.Header.t -> ?code:Cohttp.Code.status_code -> unit -> t
   val string_body :
     ?headers:Cohttp.Header.t ->
@@ -22,23 +27,25 @@ module Response : sig
 end
 
 module Handler : sig
-  type t = Request.t -> Response.t Async.Std.Deferred.t
-  val call : ('a -> 'b) -> 'a -> 'b
-  val default : 'a -> Response.t Async_core.Deferred.t
-  val not_found : 'a -> Response.t Async_core.Deferred.t
+  type t = Request.t -> Response.t Deferred.t
+  val call : t -> Request.t -> Response.t Deferred.t
+  val default : t
+  val not_found : t
 end
 
 module Middleware : sig
   type t = Handler.t -> Handler.t
-  val apply_middlewares : ('a -> 'a) Core.Std.List.t -> 'a -> 'a
+  val apply_middlewares : t list -> t
 end
 
 module App : sig
-  type t = { middlewares : Middleware.t list; handler : Handler.t; }
+  type t = {
+    middlewares : Middleware.t list;
+    handler : Handler.t;
+  } with fields
+
   val create : ?middlewares:Middleware.t list -> handler:Handler.t -> t
-  val run :
-    t ->
-    port:int ->
+  val run : t -> port:int ->
     (Async_extra.Import.Socket.Address.Inet.t, int) Cohttp_async.Server.t
-      Async.Std.Deferred.t
+      Deferred.t
 end
