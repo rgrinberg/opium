6dcdf8c349bea52d21119a89d537edadabd67b48
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-15 23:13:45 -0800

    clean up sample

--- examples/sample.ml
+++ examples/sample.ml
@@ -1,9 +1,7 @@
 open Core.Std
 open Async.Std
+open Async_unix.Async_print
 open Opium.Std
-open Cow
-
-let param = Router.param
 
 let e1 = get "/version" (fun req -> Response.respond (`String "testing"))
 
@@ -15,16 +13,18 @@ let e3 = get "/xxx/:x/:y/?" begin fun req ->
     let x = "x" |> param req |> Int.of_string in
     let y = "y" |> param req |> Int.of_string in
     let sum = Float.of_int (x + y) in
-    Response.respond @@ `Json (Json.Float sum)
+    Response.respond @@ `Json (Cow.Json.Float sum)
   end
 
 let e4 = put "/hello/:x/from/:y" begin fun req ->
-  let (x,y) = (param req "x", param req "y") in
-  let msg = sprintf "Hello %s! from %s." x y in
-  Response.respond @@ `String msg
-end
+    let (x,y) = (param req "x", param req "y") in
+    let msg = sprintf "Hello %s! from %s." x y in
+    Response.respond @@ `String msg
+  end
 
-(* test debug *)
-let throws = get "/yyy" (fun req -> List.hd_exn [])
+(* exceptions should be nicely formatted *)
+let throws = get "/yyy" (fun req ->
+    Log.Global.info "Crashing...";
+    List.hd_exn [])
 
 let _ = start [e1; e2; e3; e4; throws]
