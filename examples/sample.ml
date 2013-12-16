5df97c0466916499754be9be291fe6a00a043b02
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-16 10:09:55 -0800

    reworked sample for re-org

--- examples/sample.ml
+++ examples/sample.ml
@@ -1,25 +1,24 @@
 open Core.Std
 open Async.Std
-open Async_unix.Async_print
 open Opium.Std
 
-let e1 = get "/version" (fun req -> Response.respond (`String "testing"))
+let e1 = get "/version" (fun req -> respond (`String "testing"))
 
 let e2 = get "/hello/:name" (fun req -> 
     let name = param req "name" in
-    Response.respond @@ `String ("hello " ^ name))
+    respond @@ `String ("hello " ^ name))
 
 let e3 = get "/xxx/:x/:y/?" begin fun req ->
     let x = "x" |> param req |> Int.of_string in
     let y = "y" |> param req |> Int.of_string in
     let sum = Float.of_int (x + y) in
-    Response.respond @@ `Json (Cow.Json.Float sum)
+    respond @@ `Json (Cow.Json.Float sum)
   end
 
 let e4 = put "/hello/:x/from/:y" begin fun req ->
     let (x,y) = (param req "x", param req "y") in
     let msg = sprintf "Hello %s! from %s." x y in
-    Response.respond @@ `String msg
+    respond @@ `String msg
   end
 
 (* exceptions should be nicely formatted *)
