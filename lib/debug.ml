4fbac46d2a121c94ec4ef0516be70242e8c8c207
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-15 16:41:56 -0800

    fix debug middleware

--- lib/debug.ml
+++ lib/debug.ml
@@ -4,11 +4,11 @@ open Async.Std
 open Rock
 
 let exn_ e = Log.Global.error "%s" (Exn.to_string e)
+
 let m handler req =
-  try_with (fun () -> handler req) >>= function
+  try_with (fun () -> Handler.call handler req) >>= function
   | Ok v -> return v
   | Error _exn ->
     exn_ _exn;
-    let body = sprintf "<pre>%s</pre>" (Exn.to_string _exn)
-               |> Pipe_extra.singleton in
-    return @@ Response.create ~code:`Internal_server_error ~body
+    let body = sprintf "<pre>%s</pre>" (Exn.to_string _exn) in
+    return @@ Response.string_body ~code:`Internal_server_error body
