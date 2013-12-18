1809fd9369a21b6bb134a66d3403998fcbdf66b3
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-17 23:55:12 -0800

    2 endpoints to test cookies

--- examples/sample.ml
+++ examples/sample.ml
@@ -21,9 +21,24 @@ let e4 = put "/hello/:x/from/:y" begin fun req ->
     respond @@ `String msg
   end
 
+let set_cookie = get "/set/:key/:value" begin fun req ->
+    Log.Global.info "Setting cookie";
+    let (key, value) = (param req "key", param req "value") in
+    Cookie.set req key value;
+    respond @@ `String (Printf.sprintf "Set %s to %s" key value)
+  end
+
+let get_cookie = get "/get/:key" begin fun req ->
+    Log.Global.info "Getting cookie";
+    let key = param req "key" in
+    let message = sprintf "Cookie %s doesn't exist" key in
+    let value = Option.value_exn ~message (Cookie.get req ~key) in
+    respond @@ `String (sprintf "Cookie %s is: %s" key value) 
+  end
+
 (* exceptions should be nicely formatted *)
 let throws = get "/yyy" (fun req ->
     Log.Global.info "Crashing...";
     List.hd_exn [])
 
-let _ = start [e1; e2; e3; e4; throws]
+let _ = start [e1; e2; e3; e4; get_cookie; set_cookie; throws]
