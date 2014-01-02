bc1743ec6f8eecc5bd91685606a651fec2db2cae
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-01 20:49:18 -0800

    comment on cookie encoding used

--- lib/cookie.mli
+++ lib/cookie.mli
@@ -1,4 +1,5 @@
-
+(* Simple cookie module.  Cookies are base64'd and percent encoded
+*)
 val cookies : Rock.Request.t -> Cohttp.Cookie.cookie list
 val get : Rock.Request.t -> key:string -> string option
 val set : Rock.Request.t -> key:string -> data:string -> unit
