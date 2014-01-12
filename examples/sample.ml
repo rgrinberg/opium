106a41004faa9a7653b4031ce952e11720b5b3ca
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-12 15:45:45 -0800

    update sample based on new cookie and respond api

--- examples/sample.ml
+++ examples/sample.ml
@@ -2,29 +2,31 @@ open Core.Std
 open Async.Std
 open Opium.Std
 
-let e1 = get "/version" (fun req -> respond (`String "testing"))
+let e1 = get "/version" (fun req -> return @@ respond (`String "testing"))
 
 let e2 = get "/hello/:name" (fun req -> 
     let name = param req "name" in
-    respond @@ `String ("hello " ^ name))
+    return @@ respond @@ `String ("hello " ^ name))
 
 let e3 = get "/xxx/:x/:y/?" begin fun req ->
     let x = "x" |> param req |> Int.of_string in
     let y = "y" |> param req |> Int.of_string in
     let sum = Float.of_int (x + y) in
-    respond @@ `Json (Cow.Json.Float sum)
+    return @@ respond @@ `Json (Cow.Json.Float sum)
   end
 
 let e4 = put "/hello/:x/from/:y" begin fun req ->
     let (x,y) = (param req "x", param req "y") in
     let msg = sprintf "Hello %s! from %s." x y in
-    respond @@ `String msg
+    `String msg |> respond |> return
   end
 
 let set_cookie = get "/set/:key/:value" begin fun req ->
     let (key, value) = (param req "key", param req "value") in
-    Cookie.set req key value;
-    respond @@ `String (Printf.sprintf "Set %s to %s" key value)
+    `String (Printf.sprintf "Set %s to %s" key value)
+    |> respond
+    |> Cookie.set ~key ~data:value
+    |> return
   end
 
 let get_cookie = get "/get/:key" begin fun req ->
@@ -32,7 +34,7 @@ let get_cookie = get "/get/:key" begin fun req ->
     let key = param req "key" in
     let message = sprintf "Cookie %s doesn't exist" key in
     let value = Option.value_exn ~message (Cookie.get req ~key) in
-    respond @@ `String (sprintf "Cookie %s is: %s" key value) 
+    `String (sprintf "Cookie %s is: %s" key value) |> respond |> return
   end
 
 let all_cookies = get "/cookies" begin fun req ->
@@ -41,20 +43,20 @@ let all_cookies = get "/cookies" begin fun req ->
                   |> List.map ~f:(fun (k,v) -> k ^ "=" ^ v)
                   |> String.concat ~sep:"\n"
     in
-    respond @@ `String (sprintf "<pre>%s</pre>" cookies)
+    `String (sprintf "<pre>%s</pre>" cookies) |> respond |> return
   end
 
 (* exceptions should be nicely formatted *)
 let throws = get "/yyy" (fun req ->
     Log.Global.info "Crashing...";
-    List.hd_exn [])
+    failwith "expected failure!")
 
 let _ = start ~extra_middlewares:[Cookie.m]
-    [ e1
+    ([ e1
     ; e2
     ; e3
     ; e4
     ; get_cookie
     ; set_cookie
     ; all_cookies
-    ; throws ]
+    ; throws ])
