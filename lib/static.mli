dd721897bc36fdaf9737c2e613c4404325264f3a
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-13 22:31:17 -0800

    don't export all that unneeded stuff in static.mli

--- lib/static.mli
+++ lib/static.mli
@@ -1,17 +1,4 @@
 open Core.Std
 open Async.Std
 
-open Rock
-
-type t = {
-  prefix: string;
-  local_path: string;
-} with fields
-
-val error_body_default : string
-
-val legal_path : t -> string -> string option
-
-val public_serve : t -> requested:string -> Response.t Deferred.t
-
-val m : local_path:string -> uri_prefix:string -> Middleware.t
+val m : local_path:string -> uri_prefix:string -> Rock.Middleware.t
