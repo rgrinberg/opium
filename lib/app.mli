ecca797db17cf544169c60b70dc05fc9e508b7c5
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-13 22:35:46 -0800

    simplify exported function of app.mli

--- lib/app.mli
+++ lib/app.mli
@@ -2,8 +2,7 @@ open Core.Std
 open Async.Std
 open Rock
 
-type 'a t
-type 'a builder
+type builder
 
 val param : Request.t -> string -> string
 val respond : ?headers:Cohttp.Header.t -> ?code:Cohttp.Code.status_code ->
@@ -12,11 +11,19 @@ val respond : ?headers:Cohttp.Header.t -> ?code:Cohttp.Code.status_code ->
   | `String of string
   | `Xml of Cow.Xml.t ] -> Response.t
 
-val get : string -> 'a -> 'a builder
-val post : string -> 'a -> 'a builder
-val delete : string -> 'a -> 'a builder
-val put : string -> 'a -> 'a builder
+val respond' : ?headers:Cohttp.Header.t -> ?code:Cohttp.Code.status_code ->
+  [< `Html of Cow.Html.t
+  | `Json of Cow.Json.t
+  | `String of string
+  | `Xml of Cow.Xml.t ] -> Response.t Deferred.t
+
+type route = string -> Handler.t -> builder
+
+val get : route
+val post : route
+val delete : route
+val put : route
 
 val start : ?verbose:bool -> ?debug:bool -> ?port:int
   -> ?extra_middlewares:(Rock.Middleware.t list)
-  -> Handler.t builder list -> never_returns
+  -> builder list -> never_returns
