efeee8bc4c018175e135e7fe2aaf333af194a159
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-16 22:23:05 -0800

    travis

---
+++ .travis-ci.sh
@@ -0,0 +1,24 @@
+case "$OCAML_VERSION,$OPAM_VERSION" in
+    4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+    *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libssl-dev
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+# opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
+opam init
+sh ./deps
+
+eval `opam config env`
+make build
+make test
