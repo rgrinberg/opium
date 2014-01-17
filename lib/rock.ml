3ffc720766905f13a6d311e2431cd36372102633
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-16 19:26:49 -0800

    move crack to rock
    one cutesy name is enough

--- lib/rock.ml
+++ lib/rock.ml
@@ -8,6 +8,23 @@ open Cohttp
 open Crack
 module Co = Cohttp
 
+module Service = struct
+  type ('req, 'rep) t = 'req -> 'rep Deferred.t
+  let id req = return req
+end
+
+module Filter = struct
+  type ('req, 'rep, 'req_, 'rep_) t =
+    ('req, 'rep) Service.t -> ('req_, 'rep_) Service.t
+  type ('req, 'rep) simple = ('req, 'rep, 'req, 'rep) t
+  let id s = s
+  let (>>>) f1 f2 s = s |> f1 |> f2
+  let apply_all filters service =
+    List.fold_left filters ~init:service ~f:(|>)
+  let apply_all' filters service =
+    Array.fold filters ~init:service ~f:(|>)
+end
+
 module Request = struct
   type t = {
     request: Cohttp.Request.t;
--- lib/rock.mli
+++ lib/rock.mli
@@ -1,6 +1,32 @@
 open Core.Std
 open Async.Std
-open Crack
+
+module Service : sig
+  type ('req, 'rep) t = 'req -> 'rep Deferred.t
+
+  val id : ('a, 'a) t
+end
+
+module Filter : sig
+  type ('req, 'rep, 'req', 'rep') t =
+    ('req, 'rep) Service.t -> ('req', 'rep') Service.t
+
+  type ('req, 'rep) simple = ('req, 'rep, 'req, 'rep) t
+
+  val id : ('req, 'rep) simple
+
+  val (>>>) : ('q1, 'p1, 'q2, 'p2) t
+    -> ('q2, 'p2, 'q3, 'p3) t
+    -> ('q1, 'p1, 'q3, 'p3) t
+
+  val apply_all : ('req, 'rep) simple List.t
+    -> ('req, 'rep) Service.t
+    -> ('req, 'rep) Service.t
+
+  val apply_all' : ('req, 'rep) simple Array.t
+    -> ('req, 'rep) Service.t
+    -> ('req, 'rep) Service.t
+end
 
 module Request : sig
   type t = {
