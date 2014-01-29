892e516d7106a631ce3c0b0f9c6650d3ec18d8ca
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-28 18:07:09 -0800

    update tests for new interface

--- lib_test/test_phys_map.ml
+++ lib_test/test_phys_map.ml
@@ -2,18 +2,18 @@ open OUnit2
 
 let test_add _ =
   let x = "testing" in
-  let s = Phys_map.add Phys_map.empty ~key:x ~data:"bar" in
+  let s = Phys_map.add_exn Phys_map.empty ~key:x ~data:"bar" in
   assert_bool "exists after adding" (Phys_map.mem s ~key:x)
 
 let test_different_mem _ =
   let x1 _ = 3 in
   let x2 _ = 3 in
-  let s = Phys_map.add Phys_map.empty ~key:x2 ~data:"foo" in
+  let s = Phys_map.(add_exn empty ~key:x2 ~data:"foo") in
   assert_bool "clone doesnt exist" (not @@ Phys_map.mem s ~key:x1)
 
 let test_find _ =
   let s = "testing" in
-  let m = Phys_map.(empty |> add ~key:s ~data:"foobar") in
+  let m = Phys_map.(empty |> add_exn ~key:s ~data:"foobar") in
   assert_equal (Phys_map.find m ~key:s) (Some "foobar")
 
 let test_fixtures =
