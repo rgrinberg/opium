9a81e160d50885f298df05413e9538ef307cbdbc
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-01-28 17:58:59 -0800

    propose better interface for phys_set

--- opium/phys_map.mli
+++ opium/phys_map.mli
@@ -3,5 +3,6 @@ type 'a t
 val empty : _ t
 val mem : _ t -> key:_ -> bool
 val remove : 'a t -> key:_ -> 'a t
-val add : 'a t -> key:_ -> data:'a -> 'a t
+val add : 'a t -> key:_ -> data:'a -> [`Ok of 'a t | `Duplicate]
+val add_exn : 'a t -> key:_ -> data:'a -> 'a t
 val find : 'a t -> key:_ -> 'a option
