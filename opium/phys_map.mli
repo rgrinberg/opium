b5176f51b738302ba5e11a47157cdcf9e2cefc03
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-02 16:07:42 -0800

    remove phys map
    just add the name to middleware itself

--- opium/phys_map.mli
+++
@@ -1,8 +0,0 @@
-(** A polymorphic set module that uses physical equality *)
-type 'a t
-val empty : _ t
-val mem : _ t -> key:_ -> bool
-val remove : 'a t -> key:_ -> 'a t
-val add : 'a t -> key:_ -> data:'a -> [`Ok of 'a t | `Duplicate]
-val add_exn : 'a t -> key:_ -> data:'a -> 'a t
-val find : 'a t -> key:_ -> 'a option
