ce691cfd498698e64c4f4dee2803d0b5cdd17a83
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-03 18:32:45 -0800

    be a little more conservative in what we export in std

--- opium/opium.ml
+++ opium/opium.ml
@@ -1,11 +1,22 @@
 open Core.Std
 open Async.Std
 
+(** Open this module where you define your application's routes *)
 module Std = struct
   module Response = Rock.Response
   module Request = Rock.Request
   module Rock = Rock
   module Cookie = Cookie
   module Static = Static
-  include App
+  module App = App
+
+  (* selectively export the most useful parts of App *)
+  let param = App.param
+  let respond = App.respond
+  let respond' = App.respond'
+
+  let get = App.get
+  let post = App.post
+  let put = App.put
+  let delete = App.delete
 end
