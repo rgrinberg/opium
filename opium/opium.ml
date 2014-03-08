57404693ccc792f22185f786e846eb18749cbb83
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-07 21:04:26 -0800

    reorg Opium module. do not force to open Std

--- opium/opium.ml
+++ opium/opium.ml
@@ -1,7 +1,13 @@
-open Core.Std
-open Async.Std
+module Response = Rock.Response
+module Request = Rock.Request
+module Rock = Rock
+module Cookie = Cookie
+module Static = Static
+module App = App
 
-(** Open this module where you define your application's routes *)
+(** Open this module where you define your application's
+    routes. Opening this module's namespace is NOT necessary to use
+    this library. The main point is convenience in common operations *)
 module Std = struct
   module Response = Rock.Response
   module Request = Rock.Request
@@ -9,7 +15,6 @@ module Std = struct
   module Cookie = Cookie
   module Static = Static
   module App = App
-
   (* selectively export the most useful parts of App *)
   let param = App.param
   let respond = App.respond
