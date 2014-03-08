2df74449017bc0151afa971e0a84b0494587245d
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-08 13:50:54 -0800

    comment why some function is exported

--- opium/router.mli
+++ opium/router.mli
@@ -9,10 +9,12 @@ val add : 'a t -> Cohttp.Code.meth -> 'a -> unit
 module Route : sig
   type t
   val create : string -> t
+  val match_url : t -> string -> (string * string) list option
+
+  (** Exported for testing only *)
   val get_named_matches : ?rex:t
     -> ?pat:string
     -> string -> (string * string) list
-  val match_url : t -> string -> (string * string) list option
 end
 
 type 'action endpoint with sexp
