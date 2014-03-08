e54ee2b2f063dcec54c7ab42580a9118fade8788
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-07 21:17:58 -0800

    turn comments into ocamldoc

--- rock/rock.mli
+++ rock/rock.mli
@@ -1,3 +1,7 @@
+(** A tiny clone of ruby's Rack protocol in OCaml. Which is slightly
+    more general and inspired by Finagle. It's not imperative to have
+    this to for such a tiny framework but it makes extensions a lot
+    more straightforward *)
 open Core.Std
 open Async.Std
 
