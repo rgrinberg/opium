4823613cfeff9e99684e273ac393ea4ffbaab721
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-09 22:10:11 -0700

    move around functions in mli

--- opium/app.mli
+++ opium/app.mli
@@ -5,13 +5,36 @@ open Rock
 (** An opium app is a simple builder wrapper around a rock app *)
 type t with sexp_of
 
-(** empty app skeleton *)
+(** Functions for constructing an opium app *)
 val app : t
 
 type builder = t -> t with sexp_of
 
 val port : int -> builder
 
+type route = string -> Handler.t -> builder with sexp_of
+
+val get : route
+val post : route
+val delete : route
+val put : route
+
+val patch : route
+val options : route
+val head : route
+
+val action : Cohttp.Code.meth -> route
+
+(** Convert an opium app to a rock app  *)
+val create : t -> Rock.App.t
+
+val middleware : Middleware.t -> builder
+
+val start : t -> never_returns
+
+val command : ?name:string -> Rock.App.t -> Command.t
+
+(** Convenience functions for a running opium app *)
 type body = [
   | `Html of Cow.Html.t
   | `Json of Cow.Json.t
@@ -29,24 +52,3 @@ val respond' : ?headers:Cohttp.Header.t
   -> ?code:Cohttp.Code.status_code
   -> body
   -> Response.t Deferred.t
-
-type route = string -> Handler.t -> builder with sexp_of
-
-val get : route
-val post : route
-val delete : route
-val put : route
-
-val patch : route
-val options : route
-val head : route
-
-val action : Cohttp.Code.meth -> route
-
-val create : t -> Rock.App.t
-
-val middleware : Middleware.t -> builder
-
-val start : t -> never_returns
-
-val command : ?name:string -> Rock.App.t -> Command.t
