df9dc2f5b1513b2cbfdba0290f0af5f646a9c63a
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-16 14:57:17 -0700

    add some docs

--- opium/app.mli
+++ opium/app.mli
@@ -1,3 +1,10 @@
+(** An opium app provides a set of convenience functions and types to construct
+    a rock app.
+
+    - Re-exporting common functions needed in handlers
+    - Easy handling of routes and bodies
+    - Automatic generation of a command line app
+*)
 open Core.Std
 open Async.Std
 open Rock
@@ -5,9 +12,12 @@ open Rock
 (** An opium app is a simple builder wrapper around a rock app *)
 type t with sexp_of
 
-(** Functions for constructing an opium app *)
+(** A basic empty app *)
 val app : t
 
+(** A builder is a function that transforms an [app] by adding some
+    functionality. Builders are usuallys composed with a base app
+    using (|>) to create a full app *)
 type builder = t -> t with sexp_of
 
 val port : int -> builder
@@ -25,10 +35,10 @@ val head : route
 
 val action : Cohttp.Code.meth -> route
 
-(** Convert an opium app to a rock app  *)
-val create : t -> Rock.App.t
-
 val middleware : Middleware.t -> builder
+
+(** Convert an opium app to a rock app *)
+val create : t -> Rock.App.t
 
 val start : t -> never_returns
 
@@ -49,6 +59,7 @@ val respond : ?headers:Cohttp.Header.t
   -> body
   -> Response.t
 
+(* Same as return (respond ...) *)
 val respond' : ?headers:Cohttp.Header.t
   -> ?code:Cohttp.Code.status_code
   -> body
