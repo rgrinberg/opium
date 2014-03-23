ec7a71885eacc67de76ce2c29b85d1c015c7abfe
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-23 05:44:44 -0700

    remove some code duplication in opium.ml

--- opium/opium.ml
+++ opium/opium.ml
@@ -1,16 +1,4 @@
-module Response        = Rock.Response
-module Request         = Rock.Request
-module Rock            = Rock
-module Cookie          = Cookie
-module Router          = Router
-module Middleware_pack = Middleware_pack
-module App             = App
-
-(** Open this module where you define your application's
-    routes. Opening this module's namespace is NOT necessary to use
-    this library. The main point is convenience in common
-    operations *)
-module Std = struct
+module Opium_raw = struct
   module Response        = Rock.Response
   module Request         = Rock.Request
   module Rock            = Rock
@@ -18,6 +6,15 @@ module Std = struct
   module Router          = Router
   module Middleware_pack = Middleware_pack
   module App             = App
+end
+include Opium_raw
+
+(** Open this module where you define your application's
+    routes. Opening this module's namespace is NOT necessary to use
+    this library. The main point is convenience in common
+    operations *)
+module Std = struct
+  include Opium_raw
   (* selectively export the most useful parts of App *)
   let param    = App.param
   let respond  = App.respond
