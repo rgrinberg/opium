def5e1984360dd64ef1741f641352fbb3b0ff1fd
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-04-05 21:56:14 -0400

    add splats

--- opium/simple_route.ml
+++ opium/simple_route.ml
@@ -3,15 +3,17 @@ open Core.Std
 type path_segment =
   | Match of string
   | Param of string
+  | Splat
   | Slash
 with sexp
 
 type t = path_segment list with sexp
 
 let parse_param s =
-  if s = "/"
-  then Slash
-  else match String.chop_prefix s ~prefix:":" with
+  if s = "/" then Slash
+  else if s = "*" then Splat
+  else
+    match String.chop_prefix s ~prefix:":" with
     | Some s -> Param s
     | None -> Match s
 
@@ -31,7 +33,7 @@ let match_url t url =
     List.zip t path |> Option.map ~f:(fun l ->
       l |> List.filter_map ~f:(function
         | (Match x), y when x = y -> None
-        | Slash, "" -> None
+        | Slash, "" | Splat, _ -> None
         | (Match _), _ | Slash, _ -> raise Exit
         | (Param name), value -> Some (name, value)))
   with Exit -> None
