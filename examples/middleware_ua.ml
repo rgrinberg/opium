03d338125ee3c73a4bd2123625401ede237ba219
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-04-07 17:37:54 -0400

    dont use module alias if not needed

--- examples/middleware_ua.ml
+++ examples/middleware_ua.ml
@@ -3,13 +3,12 @@ open Async.Std
 open Opium.Std
 (* don't open cohttp and opium since they both define
    request/response modules*)
-module Co = Cohttp
 
 let is_substring ~substring s = Pcre.pmatch ~pat:(".*" ^ substring ^ ".*") s
 
 let reject_ua ~f =
   let filter handler req =
-    match Co.Header.get (Request.headers req) "user-agent" with
+    match Cohttp.Header.get (Request.headers req) "user-agent" with
     | Some ua when f ua ->
       Log.Global.info "Rejecting %s" ua;
       `String ("Please upgrade your browser") |> respond'
