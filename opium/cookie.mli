76d70fec78e69c4d712c294a2ad519f9791e4a72
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-04-07 00:28:56 -0400

    cookie docs

--- opium/cookie.mli
+++ opium/cookie.mli
@@ -1,7 +1,13 @@
 (** Simple cookie module.  Cookies are base64'd and percent encoded.
 *)
-val cookies : Rock.Request.t -> Cohttp.Cookie.cookie list
+
+(** Fetch all cookies from a rock request *)
+val cookies : Rock.Request.t -> (string * string) list
+(** Get the follow of a cookie with a certain key *)
 val get : Rock.Request.t -> key:string -> string option
+(** Set the value of a cookie with a certain key in a response *)
 val set : Rock.Response.t -> key:string -> data:string -> Rock.Response.t
+(** Like set but will do multiple cookies at once *)
 val set_cookies : Rock.Response.t -> (string * string) list -> Rock.Response.t
+(** Rock middleware to add the the functionality above *)
 val m : Rock.Middleware.t
