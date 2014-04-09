94bd46167a1c7dc32580b468d32849ad66a084ee
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-04-08 22:46:47 -0400

    formatting

--- examples/read_json_body.ml
+++ examples/read_json_body.ml
@@ -6,8 +6,8 @@ open Opium.Std
 
 let print_json req =
   App.json_of_body_exn req >>| fun json ->
-    Log.Global.info "Received: %s" (Json.to_string json);
-    respond (`String "Received response")
+  Log.Global.info "Received: %s" (Json.to_string json);
+  respond (`String "Received response")
 
 let _ =
   App.app
