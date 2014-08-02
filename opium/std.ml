4e3e434ea3766a35570357a84aabc95a3ba522d3
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-08-02 16:43:04 -0400

    move shit around

---
+++ opium/std.ml
@@ -0,0 +1,23 @@
+module Rock            = Opium_rock
+module Response        = Rock.Response
+module Request         = Rock.Request
+module Cookie          = Cookie
+module Router          = Router
+module Middleware_pack = Middleware_pack
+module App             = App
+
+(* selectively export the most useful parts of App *)
+let param    = App.param
+let splat    = App.splat
+let respond  = App.respond
+let respond' = App.respond'
+
+let get    = App.get
+let post   = App.post
+let put    = App.put
+let delete = App.delete
+
+let all = App.all
+let any = App.any
+
+let middleware = App.middleware
