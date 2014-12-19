f0537a31eedbfdcab05c211740c51c77e26b9872
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-12-18 20:32:53 -0500

    kill cow in hello world example

--- examples/hello_world.ml
+++ examples/hello_world.ml
@@ -1,31 +1,31 @@
 open Core.Std
-(* open Cow *)
 open Opium.Std
 
-(* module Person = struct *)
-(*   (* this hack is needed because cow is relying on functions shadowed *)
-(*      by core *) *)
-(*   open Caml *)
-(*   type t = { *)
-(*     name: string; *)
-(*     age: int; } with json *)
-(* end *)
-(*  *)
-(* let print_param = put "/hello/:name" begin fun req -> *)
-(*   `String ("Hello " ^ param req "name") |> respond' *)
-(* end *)
-(*  *)
-(* let print_person = get "/person/:name/:age" begin fun req -> *)
-(*   let person = { *)
-(*     Person.name = param req "name"; *)
-(*     age = "age" |> param req |> Int.of_string; *)
-(*   } in *)
-(*   `Json (Person.json_of_t person) |> respond' *)
-(* end *)
-(*  *)
-(* let _ = *)
-(*   App.empty *)
-(*   |> print_param *)
-(*   |> print_person *)
-(*   |> App.command *)
-(*   |> Command.run *)
+type person = {
+  name: string;
+  age: int;
+}
+
+let json_of_person { name ; age } =
+  let open Ezjsonm in
+  dict [ "name", (string name)
+       ; "age", (int age) ]
+
+let print_param = put "/hello/:name" begin fun req ->
+  `String ("Hello " ^ param req "name") |> respond'
+end
+
+let print_person = get "/person/:name/:age" begin fun req ->
+  let person = {
+    name = param req "name";
+    age = "age" |> param req |> Int.of_string;
+  } in
+  `Json (person |> json_of_person |> Ezjsonm.wrap ) |> respond'
+end
+
+let _ =
+  App.empty
+  |> print_param
+  |> print_person
+  |> App.command
+  |> Command.run
