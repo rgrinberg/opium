856ecd9d23cfc906751936e89c9d3523302cc63c
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-12-19 22:01:29 -0500

    remove functorizing

--- opium/app.ml
+++ opium/app.ml
@@ -4,41 +4,40 @@ open Opium_misc
 module Rock = Opium_rock
 open Rock
 
-module Make (Router : App_intf.Router) = struct
-  type t = {
-    port:        int;
-    debug:       bool;
-    verbose:     bool;
-    routes :     (Co.Code.meth * Router.Route.t * Handler.t) list;
-    middlewares: Middleware.t list;
-    name:        string;
-    not_found :  Handler.t;
-  } with fields, sexp_of
+type t = {
+  port:        int;
+  debug:       bool;
+  verbose:     bool;
+  routes :     (Co.Code.meth * Router.Route.t * Handler.t) list;
+  middlewares: Middleware.t list;
+  name:        string;
+  not_found :  Handler.t;
+} with fields, sexp_of
 
-  type builder = t -> t with sexp_of
+type builder = t -> t with sexp_of
 
-  type route = string -> Handler.t -> builder with sexp_of
+type route = string -> Handler.t -> builder with sexp_of
 
-  let register app ~meth ~route ~action =
-    { app with routes=(meth, route, action)::app.routes }
+let register app ~meth ~route ~action =
+  { app with routes=(meth, route, action)::app.routes }
 
-  let empty =
-    { name        = "Opium Default Name";
-      port        = 3000;
-      debug       = false;
-      verbose     = false;
-      routes      = [];
-      middlewares = [];
-      not_found   = Handler.not_found }
+let empty =
+  { name        = "Opium Default Name";
+    port        = 3000;
+    debug       = false;
+    verbose     = false;
+    routes      = [];
+    middlewares = [];
+    not_found   = Handler.not_found }
 
-  let create_router routes =
-    let router = Router.create () in
-    routes
-    |> List.iter ~f:(fun (meth, route, action) ->
-      Router.add router ~meth ~route ~action);
-    router
+let create_router routes =
+  let router = Router.create () in
+  routes
+  |> List.iter ~f:(fun (meth, route, action) ->
+    Router.add router ~meth ~route ~action);
+  router
 
-  let attach_middleware { verbose ; debug ; routes ; middlewares ; _  } =
+let attach_middleware { verbose ; debug ; routes ; middlewares ; _  } =
   [ Some (routes |> create_router |> Router.m) ] @
   (List.map ~f:Option.some middlewares) @
   [
@@ -46,173 +45,171 @@ module Make (Router : App_intf.Router) = struct
     (if debug then Some Middleware_pack.debug else None);
   ] |> List.filter_opt
 
-  let port port t = { t with port }
-  let cmd_name name t = { t with name }
+let port port t = { t with port }
+let cmd_name name t = { t with name }
 
-  let middleware m app =
-    { app with middlewares=m::app.middlewares }
+let middleware m app =
+  { app with middlewares=m::app.middlewares }
 
-  let public_path root requested =
-    let asked_path = Filename.concat root requested in
-    Option.some_if (String.is_prefix asked_path ~prefix:root) asked_path
+let public_path root requested =
+  let asked_path = Filename.concat root requested in
+  Option.some_if (String.is_prefix asked_path ~prefix:root) asked_path
 
-  let action meth route action =
-    register ~meth ~route:(Router.Route.of_string route) ~action
+let action meth route action =
+  register ~meth ~route:(Router.Route.of_string route) ~action
 
-  let get route action =
-    register ~meth:`GET ~route:(Router.Route.of_string route) ~action
-  let post route action =
-    register ~meth:`POST ~route:(Router.Route.of_string route) ~action
-  let delete route action =
-    register ~meth:`DELETE ~route:(Router.Route.of_string route) ~action
-  let put route action =
-    register ~meth:`PUT ~route:(Router.Route.of_string route) ~action
+let get route action =
+  register ~meth:`GET ~route:(Router.Route.of_string route) ~action
+let post route action =
+  register ~meth:`POST ~route:(Router.Route.of_string route) ~action
+let delete route action =
+  register ~meth:`DELETE ~route:(Router.Route.of_string route) ~action
+let put route action =
+  register ~meth:`PUT ~route:(Router.Route.of_string route) ~action
 
-  let patch route action =
-    register ~meth:`PATCH ~route:(Router.Route.of_string route) ~action
-  let head route action =
-    register ~meth:`HEAD ~route:(Router.Route.of_string route) ~action
-  let options route action =
-    register ~meth:`OPTIONS ~route:(Router.Route.of_string route) ~action
+let patch route action =
+  register ~meth:`PATCH ~route:(Router.Route.of_string route) ~action
+let head route action =
+  register ~meth:`HEAD ~route:(Router.Route.of_string route) ~action
+let options route action =
+  register ~meth:`OPTIONS ~route:(Router.Route.of_string route) ~action
 
-  let any methods route action t =
-    (if List.is_empty methods then
-       Lwt_log.ign_warning_f
-         "Warning: you're using [any] attempting to bind to '%s' but your list
+let any methods route action t =
+  (if List.is_empty methods then
+     Lwt_log.ign_warning_f
+       "Warning: you're using [any] attempting to bind to '%s' but your list
         of http methods is empty route" route);
-    let route = Router.Route.of_string route in
-    methods |> List.fold_left ~init:t
-                 ~f:(fun app meth -> app |> register ~meth ~route ~action)
+  let route = Router.Route.of_string route in
+  methods |> List.fold_left ~init:t
+               ~f:(fun app meth -> app |> register ~meth ~route ~action)
 
-  let all = any [`GET;`POST;`DELETE;`PUT;`PATCH;`HEAD;`OPTIONS]
+let all = any [`GET;`POST;`DELETE;`PUT;`PATCH;`HEAD;`OPTIONS]
 
-  let to_rock app =
-    Rock.App.create ~middlewares:(attach_middleware app)
-      ~handler:(app.not_found)
+let to_rock app =
+  Rock.App.create ~middlewares:(attach_middleware app)
+    ~handler:(app.not_found)
 
-  let start app =
-    let middlewares = attach_middleware app in
-    if app.verbose then
-      Lwt_log.ign_info_f "Running on port: %d%s" app.port
-        (if app.debug then " (debug)" else "");
-    let port = app.port in
-    let app = Rock.App.create ~middlewares ~handler:app.not_found in
-    app |> Rock.App.run ~port |> Lwt_unix.run
+let start app =
+  let middlewares = attach_middleware app in
+  if app.verbose then
+    Lwt_log.ign_info_f "Running on port: %d%s" app.port
+      (if app.debug then " (debug)" else "");
+  let port = app.port in
+  let app = Rock.App.create ~middlewares ~handler:app.not_found in
+  app |> Rock.App.run ~port |> Lwt_unix.run
 
-  type 'a runner = int -> string -> bool -> bool -> bool -> bool -> bool -> bool -> 'a
-  type 'a action = (unit -> unit) runner
-  type 'a spec = ('a runner, 'a) Command.Spec.t
+type 'a runner = int -> string -> bool -> bool -> bool -> bool -> bool -> bool -> 'a
+type 'a action = (unit -> unit) runner
+type 'a spec = ('a runner, 'a) Command.Spec.t
 
-  let spec app' =
-    let summary = name app' in
-    let open Command.Spec in
-    object
-      method summary = summary
+let spec app' =
+  let summary = name app' in
+  let open Command.Spec in
+  object
+    method summary = summary
 
-      method spec =
-        empty
-        +> flag "-p" (optional_with_default 3000 int)
-             ~doc:"port number to listen"
-        +> flag "-h" (optional_with_default "0.0.0.0" string)
-             ~doc:"interface to listen"
-        +> flag "-r" no_arg ~doc: "print routes"
-        +> flag "-m" no_arg ~doc:"print middleware stack"
-        +> flag "-d" no_arg ~doc:"enable debug information"
-        +> flag "-v" no_arg ~doc:"enable verbose mode"
-        +> flag "-xi" no_arg ~doc:"Ignore errors (conflicts with -xr and -d)"
-        +> flag "-xr" no_arg ~doc:"Raise on errors (conflicts with -xi)"
+    method spec =
+      empty
+      +> flag "-p" (optional_with_default 3000 int)
+           ~doc:"port number to listen"
+      +> flag "-h" (optional_with_default "0.0.0.0" string)
+           ~doc:"interface to listen"
+      +> flag "-r" no_arg ~doc: "print routes"
+      +> flag "-m" no_arg ~doc:"print middleware stack"
+      +> flag "-d" no_arg ~doc:"enable debug information"
+      +> flag "-v" no_arg ~doc:"enable verbose mode"
+      +> flag "-xi" no_arg ~doc:"Ignore errors (conflicts with -xr and -d)"
+      +> flag "-xr" no_arg ~doc:"Raise on errors (conflicts with -xi)"
 
-      method action =
-        (fun port host print_routes print_middleware debug verbose
-          ignore_e raise_e () ->
-          let app' = { app' with debug ; verbose } in
-          let app = to_rock app' in
-          (if print_routes then begin
-             let routes_tbl = Hashtbl.Poly.create () in
-             app' |> routes |> List.iter ~f:(fun (meth, route, _) ->
-               Hashtbl.add_multi routes_tbl ~key:route ~data:meth);
-             printf "%d Routes:\n" (Hashtbl.length routes_tbl);
-             Hashtbl.iter routes_tbl ~f:(fun ~key ~data ->
-               printf "> %s (%s)\n" (Router.Route.to_string key)
-                 (data
-                  |> List.map ~f:Cohttp.Code.string_of_method
-                  |> String.concat ~sep:" ")
-             );
-             Unix.exit_immediately 0;
-           end;
-           if print_middleware then begin
-             print_endline "Active middleware:";
-             app
-             |> Rock.App.middlewares
-             |> List.map ~f:(Fn.compose Info.to_string_hum Rock.Middleware.name)
-             |> List.iter ~f:(fun name ->
-               printf "> %s \n" name);
-             Unix.exit_immediately 0
-           end
-          );
-          (if debug || verbose then
-             Lwt_log.ign_info_f "Listening on %s:%d" host port);
-          app |> Rock.App.run ~port |> Lwt_main.run
-        )
-    end
-
-  let command app =
-    let spec = spec app in
-    Command.basic ~summary:spec#summary spec#spec spec#action
-
-  type body = [
-    | `Html of string
-    | `Json of Ezjsonm.t
-    | `Xml of string
-    | `String of string ]
-
-  module Response_helpers = struct
-
-    let content_type ct = Cohttp.Header.init_with "Content-Type" ct
-    let json_header     = content_type "application/json"
-    let xml_header      = content_type "application/xml"
-    let html_header     = content_type "text/html"
-
-    let respond_with_string = Response.of_string_body
-
-    let respond ?headers ?(code=`OK) = function
-      | `String s -> respond_with_string ?headers ~code s
-      | `Json s ->
-        respond_with_string ~code ~headers:json_header (Ezjsonm.to_string s)
-      | `Html s ->
-        respond_with_string ~code ~headers:html_header s
-      | `Xml s ->
-        respond_with_string ~code ~headers:xml_header s
-
-    let respond' ?headers ?code s =
-      s |> respond ?headers ?code |> return
-
-    let redirect ?headers uri =
-      let headers = Cohttp.Header.add_opt headers "Location" (Uri.to_string uri) in
-      Response.create ~headers ~code:`Found ()
-
-    let redirect' ?headers uri =
-      uri |> redirect ?headers |> return
+    method action =
+      (fun port host print_routes print_middleware debug verbose
+        ignore_e raise_e () ->
+        let app' = { app' with debug ; verbose } in
+        let app = to_rock app' in
+        (if print_routes then begin
+           let routes_tbl = Hashtbl.Poly.create () in
+           app' |> routes |> List.iter ~f:(fun (meth, route, _) ->
+             Hashtbl.add_multi routes_tbl ~key:route ~data:meth);
+           printf "%d Routes:\n" (Hashtbl.length routes_tbl);
+           Hashtbl.iter routes_tbl ~f:(fun ~key ~data ->
+             printf "> %s (%s)\n" (Router.Route.to_string key)
+               (data
+                |> List.map ~f:Cohttp.Code.string_of_method
+                |> String.concat ~sep:" ")
+           );
+           Unix.exit_immediately 0;
+         end;
+         if print_middleware then begin
+           print_endline "Active middleware:";
+           app
+           |> Rock.App.middlewares
+           |> List.map ~f:(Fn.compose Info.to_string_hum Rock.Middleware.name)
+           |> List.iter ~f:(fun name ->
+             printf "> %s \n" name);
+           Unix.exit_immediately 0
+         end
+        );
+        (if debug || verbose then
+           Lwt_log.ign_info_f "Listening on %s:%d" host port);
+        app |> Rock.App.run ~port |> Lwt_main.run
+      )
   end
 
-  module Request_helpers = struct
-    let json_exn req =
-      req |> Request.body |> Body.to_string >>| Ezjsonm.from_string
-    let string_exn req =
-      req |> Request.body |> Body.to_string
-    let pairs_exn req =
-      req |> Request.body |> Body.to_string >>| Uri.query_of_encoded
-  end
+let command app =
+  let spec = spec app in
+  Command.basic ~summary:spec#summary spec#spec spec#action
 
-  let json_of_body_exn         = Request_helpers.json_exn
-  let string_of_body_exn       = Request_helpers.string_exn
-  let urlencoded_pairs_of_body = Request_helpers.pairs_exn
-  let param                    = Router.param
-  let splat                    = Router.splat
-  let respond                  = Response_helpers.respond
-  let respond'                 = Response_helpers.respond'
-  let redirect                 = Response_helpers.redirect
-  let redirect'                = Response_helpers.redirect'
+type body = [
+  | `Html of string
+  | `Json of Ezjsonm.t
+  | `Xml of string
+  | `String of string ]
+
+module Response_helpers = struct
+
+  let content_type ct = Cohttp.Header.init_with "Content-Type" ct
+  let json_header     = content_type "application/json"
+  let xml_header      = content_type "application/xml"
+  let html_header     = content_type "text/html"
+
+  let respond_with_string = Response.of_string_body
+
+  let respond ?headers ?(code=`OK) = function
+    | `String s -> respond_with_string ?headers ~code s
+    | `Json s ->
+      respond_with_string ~code ~headers:json_header (Ezjsonm.to_string s)
+    | `Html s ->
+      respond_with_string ~code ~headers:html_header s
+    | `Xml s ->
+      respond_with_string ~code ~headers:xml_header s
+
+  let respond' ?headers ?code s =
+    s |> respond ?headers ?code |> return
+
+  let redirect ?headers uri =
+    let headers = Cohttp.Header.add_opt headers "Location" (Uri.to_string uri) in
+    Response.create ~headers ~code:`Found ()
+
+  let redirect' ?headers uri =
+    uri |> redirect ?headers |> return
 end
 
-include Make(Router)
+module Request_helpers = struct
+  let json_exn req =
+    req |> Request.body |> Body.to_string >>| Ezjsonm.from_string
+  let string_exn req =
+    req |> Request.body |> Body.to_string
+  let pairs_exn req =
+    req |> Request.body |> Body.to_string >>| Uri.query_of_encoded
+end
+
+let json_of_body_exn         = Request_helpers.json_exn
+let string_of_body_exn       = Request_helpers.string_exn
+let urlencoded_pairs_of_body = Request_helpers.pairs_exn
+let param                    = Router.param
+let splat                    = Router.splat
+let respond                  = Response_helpers.respond
+let respond'                 = Response_helpers.respond'
+let redirect                 = Response_helpers.redirect
+let redirect'                = Response_helpers.redirect'
+
--- opium/app.mli
+++ opium/app.mli
@@ -1,3 +1,103 @@
-(** See App_intf for documentation *)
-module Make (R : App_intf.Router) : App_intf.S
-include App_intf.S
+(** An opium app provides a set of convenience functions and types to construct
+    a rock app.
+
+    - Re-exporting common functions needed in handlers
+    - Easy handling of routes and bodies
+    - Automatic generation of a command line app
+*)
+open Core.Std
+open Opium_rock
+
+(** An opium app is a simple builder wrapper around a rock app *)
+type t with sexp_of
+
+(** A basic empty app *)
+val empty : t
+
+(** A builder is a function that transforms an [app] by adding some
+    functionality. Builders are usuallys composed with a base app
+    using (|>) to create a full app *)
+type builder = t -> t with sexp_of
+
+val port : int -> builder
+
+val cmd_name : string -> builder
+
+(** A route is a function that returns a buidler that hooks up a
+    handler to a url mapping *)
+type route = string -> Handler.t -> builder with sexp_of
+
+(** Method specific routes *)
+val get : route
+val post : route
+val delete : route
+val put : route
+
+(** Less common method specific routes  *)
+val patch : route
+val options : route
+val head : route
+
+(** any [methods] will bind a route to any http method inside of
+    [methods] *)
+val any : Cohttp.Code.meth list -> route
+(** all [methods] will bind a route to a URL regardless of the http method.
+    You may escape the actual method used from the request passed. *)
+val all : route
+
+val action : Cohttp.Code.meth -> route
+
+val middleware : Middleware.t -> builder
+
+(** Convert an opium app to a rock app *)
+val to_rock : t -> Opium_rock.App.t
+
+val start : t -> unit
+
+(** Create a core command from an opp *)
+val command : t -> Command.t
+
+type 'a runner = int -> string -> bool -> bool -> bool -> bool -> bool -> bool -> 'a
+type 'a action = (unit -> unit) runner
+type 'a spec = ('a runner, 'a) Command.Spec.t
+
+(** Returns the command spec for the opium and the original runner. Used for
+ * customizing own command line options*)
+val spec: t -> < summary: string; spec: 'a Lwt.t spec; action: 'a action; >
+
+(** Convenience functions for a running opium app *)
+type body = [
+  | `Html of string
+  | `Json of Ezjsonm.t
+  | `Xml of string
+  | `String of string ]
+
+val json_of_body_exn : Request.t -> Ezjsonm.t Lwt.t
+
+val string_of_body_exn : Request.t -> string Lwt.t
+
+val urlencoded_pairs_of_body : Request.t -> (string * string list) list Lwt.t
+
+val param : Request.t -> string -> string
+
+val splat : Request.t -> string list
+
+val respond : ?headers:Cohttp.Header.t
+  -> ?code:Cohttp.Code.status_code
+  -> body
+  -> Response.t
+
+(* Same as return (respond ...) *)
+val respond' : ?headers:Cohttp.Header.t
+  -> ?code:Cohttp.Code.status_code
+  -> body
+  -> Response.t Lwt.t
+
+val redirect : ?headers:Cohttp.Header.t
+  -> Uri.t
+  -> Response.t
+
+(* Same as return (redirect ...) *)
+val redirect' : ?headers:Cohttp.Header.t
+  -> Uri.t
+  -> Response.t Lwt.t
