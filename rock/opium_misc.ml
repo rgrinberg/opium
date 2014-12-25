a5204f434c43cbda7aed3cb5262251408ded2528
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-12-24 21:53:14 -0500

    doc for opam_misc

--- rock/opium_misc.ml
+++ rock/opium_misc.ml
@@ -1,6 +1,8 @@
+(** Module with helpful aliases in transitioning from async to lwt *)
 let return = Lwt.return
 let (>>|) = Lwt.(>|=)
 let (>>=) = Lwt.(>>=)
+
 module Co     = Cohttp
 module Body   = Cohttp_lwt_body
 module Server = Cohttp_lwt_unix.Server
