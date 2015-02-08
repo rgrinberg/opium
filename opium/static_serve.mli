38d3d32236166792f914b714528754bc36ea9f32
Anil Madhavapeddy <anil@recoil.org>
2015-02-08 17:48:22 +0000

    Generate Content-Type headers for static file serving
    This uses the `magic-mime` library to detect the filename and
    generate a sensible MIME type from the `mime.types` database.
    It is particularly important to generate these for Internet Explorer
    support, since IE10 seems to ignore `.css` files that do not send
    an appropriate MIME header.  With this patch, IE10 rendering works
    great using an Opium rendering stack.

--- opium/static_serve.mli
+++ opium/static_serve.mli
@@ -1,3 +1,5 @@
-(** Middleware serves all fiels (recursively) in the [local_path] directory
-    under the [uri_prefix] url *)
+(** Middleware serves all files (recursively) in the [local_path] directory
+    under the [uri_prefix] url.  The responses contain a [Content-type]
+    header that is auto-detected based on the file extension using the
+    {!Magic_mime.lookup} function. *)
 val m : local_path:string -> uri_prefix:string -> Opium_rock.Middleware.t
