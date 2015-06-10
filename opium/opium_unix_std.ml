f2bbc54ce9fa3ca74ffbe021708fc6157947c3a6
Arnout Engelen <arnouten@bzzt.net>
2015-06-11 01:33:08 +0200

    Remove some opium_unix remnants

--- opium/opium_unix_std.ml
+++
@@ -1,5 +0,0 @@
-module Middleware = struct
-  (** Re-exports simple middleware that doesn't have auxiliary
-      functions *)
-  let static = Static_serve.m
-end
--- opium/std.ml
+++ opium/std.ml
@@ -9,6 +9,7 @@ module Debug           = Opium_kernel.Debug
 module Middleware = struct
   (** Re-exports simple middleware that doesn't have auxiliary
       functions *)
+  let static = Static_serve.m
   let debug = Debug.debug
   let trace = Debug.trace
 end
