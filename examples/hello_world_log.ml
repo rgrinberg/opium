4f69949c2c8f58d6f672ae4427f6269dd7976497
Anurag Soni <anurag@sonianurag.com>
2019-01-21 02:24:59 -0500

    Log reporter example
    This is an example of a scenario where
    the log reporter is returned via
    a Lwt promise
    
    Reference: #89, #81
    
    Signed-off-by: Anurag Soni <anurag@sonianurag.com>

---
+++ examples/hello_world_log.ml
@@ -0,0 +1,40 @@
+open Opium.Std
+open Lwt.Infix
+
+(* This is done to demonstrate a usecase where the log reporter
+   is returned via a Lwt promise *)
+let log_reporter () = Lwt.return (Logs_fmt.reporter ())
+
+let logger =
+  let filter handler req =
+    let uri = Request.uri req |> Uri.path_and_query in
+    handler req
+    >|= fun response ->
+    let code = response |> Response.code |> Cohttp.Code.code_of_status in
+    Logs.info (fun m -> m "Responded to '%s' with %d" uri code) ;
+    response
+  in
+  Rock.Middleware.create ~name:"Logger" ~filter
+
+let say_hello =
+  get "/hello/:name" (fun req ->
+      `String ("Hello " ^ param req "name") |> respond' )
+
+let () =
+  let app = App.empty
+          |> say_hello
+          |> middleware logger
+          |> App.run_command' in
+  match app with
+  | `Ok app ->
+      let s =
+        log_reporter ()
+        >>= fun r -> begin
+            Logs.set_reporter r ;
+            Logs.set_level (Some Logs.Info) ;
+            app
+          end
+      in
+      ignore (Lwt_main.run s)
+  | `Error -> exit 1
+  | `Not_running -> exit 0
