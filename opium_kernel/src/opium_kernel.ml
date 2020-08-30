3c92e0bbb2f7b9e8c98e5e1e08fc830a584cc4f0
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-08-30 15:26:58 +0200

    Add a static middleware in opium_kernel

--- opium_kernel/src/opium_kernel.ml
+++ opium_kernel/src/opium_kernel.ml
@@ -7,10 +7,12 @@ module Rock = Rock
 module Route = Route
 module Server_connection = Server_connection
 module Router = Router
+module Static = Static
 
 module Middleware = struct
   let router = Router.m
   let debugger = Debugger.m
   let logger = Logger.m
   let allow_cors = Allow_cors.m
+  let static = Static.m
 end
