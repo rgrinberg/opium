675265a329afc35f55a588f24f002a5aaf3f7e3c
Reynir Björnsson <reynir@reynir.dk>
2020-12-03 11:00:50 +0100

    Add example/json_response/README.md
    Also annotate the type of the json object.

--- example/json_response/main.ml
+++ example/json_response/main.ml
@@ -1,7 +1,7 @@
 open Opium
 
 let sys_json _req =
-  let json = `Assoc [
+  let json : Yojson.Safe.t = `Assoc [
       "os-type", `String Sys.os_type;
       "ocaml-version", `String Sys.ocaml_version;
     ]
