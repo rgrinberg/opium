d7d56668ddcf300eb4655ea378882c09c5efefac
Reynir Björnsson <reynir@reynir.dk>
2020-12-03 10:37:28 +0100

    Add example/json_response/

---
+++ example/json_response/main.ml
@@ -0,0 +1,11 @@
+open Opium
+
+let sys_json _req =
+  let json = `Assoc [
+      "os-type", `String Sys.os_type;
+      "ocaml-version", `String Sys.ocaml_version;
+    ]
+  in
+  Response.of_json json |> Lwt.return
+
+let _ = App.empty |> App.get "/" sys_json |> App.run_command
